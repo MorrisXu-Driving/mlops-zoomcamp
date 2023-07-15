fd917bbbea78197850a07bc8adbb9a8703e71792
MorrisXu <s1052262195@gmail.com>
2023-07-15 14:11:57 +0800

    update changed score.py

--- 04-deployment/batch/score.py
+++ 04-deployment/batch/score.py
@@ -56,7 +56,7 @@ def prepare_dictionaries(df: pd.DataFrame):
 
 
 def load_model(run_id):
-    logged_model = f's3://mlflow-models-alexey/1/{run_id}/artifacts/model'
+    logged_model = f's3://mlflow-artifacts-remote-morrisxu/1/{run_id}/artifacts/model'
     model = mlflow.pyfunc.load_model(logged_model)
     return model
 
@@ -100,8 +100,8 @@ def get_paths(run_date, taxi_type, run_id):
     year = prev_month.year
     month = prev_month.month 
 
-    input_file = f's3://nyc-tlc/trip data/{taxi_type}_tripdata_{year:04d}-{month:02d}.parquet'
-    output_file = f's3://nyc-duration-prediction-alexey/taxi_type={taxi_type}/year={year:04d}/month={month:02d}/{run_id}.parquet'
+    input_file = f'https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_{year:04d}-{month:02d}.parquet'
+    output_file = f's3://mlflow-artifacts-remote-morrisxu/taxi_type={taxi_type}/year={year:04d}/month={month:02d}/{run_id}.parquet'
 
     return input_file, output_file
 
@@ -129,7 +129,7 @@ def run():
     year = int(sys.argv[2]) # 2021
     month = int(sys.argv[3]) # 3
 
-    run_id = sys.argv[4] # 'e1efc53e9bd149078b0c12aeaa6365df'
+    run_id = '716a1ded1c9d44bca420809f492f67aa'
 
     ride_duration_prediction(
         taxi_type=taxi_type,
